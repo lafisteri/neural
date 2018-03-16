f43c4cd368f7f6699db3ba15ecd4bdf2196e3091
lafisteri <nasulichanton@gmail.com>
2018-03-16 18:26:55 +0200

    query() was described

--- network.py
+++ network.py
@@ -9,16 +9,41 @@ class neuralNetwork:
 
         self.learningRate = learningrate
 
-        #self.wih = numpy.random.rand(self.hidenNodes,self.inputNodes) - 0.5
-        #self.who = numpy.random.rand(self.outputNodes,self.hidenNodes) - 0.5
         self.wih = numpy.random.normal(0.0, pow(self.hidenNodes, -0.5), (self.hidenNodes, self.inputNodes))
         self.who = numpy.random.normal(0.0, pow(self.outputNodes, -0.5), (self.outputNodes, self.hidenNodes))
 
+        #использование сигмоиды в качестве функции активации
         self.activation_function = lambda x: scipy.special.expit(x)
         pass
 
-    def train(self):
+    def train(self, input_list, targets_list):
+        # преобразовать список входящих значений в двумерный массив
+        inputs = numpy.array(input_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
 
+        #расчитать входящие сигналы для скрытого слоя
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        #расчитать исходящие сигналы для скрытого слоя
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        #расчитать входящие сигналы для выходного слоя
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        #расчитать исходящие сигналы для выходного слоя
+        final_outputs = self.activation_function(final_inputs)
+
+        #щшибки выходного слоя = целевое значение - фактическое значение
+        output_errors = targets - final_outputs
+        #дшибки скрытого слоя это ошибки output_errors расределенные пропорционально
+        # весовым коэфициентам связей и рекомбинированы на скрытых узлах
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        #обновить весовые коэффициенты для связей между скрытым и выходным слоями
+        self.who += self.learningRate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                                  numpy.transpose(hidden_outputs))
+
+        #обновить весовые коэфициенты для связей между входным и скрытым слоями
+        self.wih += self.learningRate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                                  numpy.transpose(hidden_outputs))
         pass
 
     def query(self, inputs_list):
