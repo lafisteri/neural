9d996355423804fffd3b6a8696c4c447eeb42848
lafisteri <nasulichanton@gmail.com>
2018-03-16 23:35:12 +0200

    - add train data - display train data.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import  numpy
 from network import neuralNetwork
+import  numpy
+import matplotlib.pyplot as plt
+from IPython import get_ipython
+ipython = get_ipython()
 
-input_nodes = 3
+
+
+
+"""input_nodes = 3
 hiden_nodes = 3
 output_nodes = 3
 learning_rate = 0.3
-
 n = neuralNetwork(input_nodes, hiden_nodes, output_nodes, learning_rate)
+print(numpy.random.rand(3,3) - 0.5)
+print(n.query([1.0, 0.5, -1.5]))"""
 
-#print(numpy.random.rand(3,3) - 0.5)
 
 
-print(n.query([1.0, 0.5, -1.5]))
+
+data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
+data_list = data_file.readlines();
+data_file.close()
+
+all_values = data_list[0].split(',')
+image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+plt.imshow(image_array, cmap='Greys', interpolation='None')
+plt.show(block=True)
--- network.py
+++ network.py
@@ -2,17 +2,17 @@ import numpy
 import  scipy.special
 
 class neuralNetwork:
-    def __init__(self, inputnodes, hidennodes, outputnodes, learningrate):
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
         self.inputNodes = inputnodes
-        self.hidenNodes = hidennodes
+        self.hiddenNodes = hiddennodes
         self.outputNodes = outputnodes
 
         self.learningRate = learningrate
 
-        self.wih = numpy.random.normal(0.0, pow(self.hidenNodes, -0.5), (self.hidenNodes, self.inputNodes))
-        self.who = numpy.random.normal(0.0, pow(self.outputNodes, -0.5), (self.outputNodes, self.hidenNodes))
+        self.wih = numpy.random.normal(0.0, pow(self.hiddenNodes, -0.5), (self.hiddenNodes, self.inputNodes))
+        self.who = numpy.random.normal(0.0, pow(self.outputNodes, -0.5), (self.outputNodes, self.hiddenNodes))
 
-        #использование сигмоиды в качестве функции активации
+        # использование сигмоиды в качестве функции активации
         self.activation_function = lambda x: scipy.special.expit(x)
         pass
 
@@ -21,47 +21,46 @@ class neuralNetwork:
         inputs = numpy.array(input_list, ndmin=2).T
         targets = numpy.array(targets_list, ndmin=2).T
 
-        #расчитать входящие сигналы для скрытого слоя
+        # расчитать входящие сигналы для скрытого слоя
         hidden_inputs = numpy.dot(self.wih, inputs)
-        #расчитать исходящие сигналы для скрытого слоя
+        # расчитать исходящие сигналы для скрытого слоя
         hidden_outputs = self.activation_function(hidden_inputs)
 
-        #расчитать входящие сигналы для выходного слоя
+        # расчитать входящие сигналы для выходного слоя
         final_inputs = numpy.dot(self.who, hidden_outputs)
-        #расчитать исходящие сигналы для выходного слоя
+        # расчитать исходящие сигналы для выходного слоя
         final_outputs = self.activation_function(final_inputs)
 
-        #щшибки выходного слоя = целевое значение - фактическое значение
+        # ошибки выходного слоя = целевое значение - фактическое значение
         output_errors = targets - final_outputs
-        #дшибки скрытого слоя это ошибки output_errors расределенные пропорционально
+        # ошибки скрытого слоя это ошибки output_errors расределенные пропорционально
         # весовым коэфициентам связей и рекомбинированы на скрытых узлах
         hidden_errors = numpy.dot(self.who.T, output_errors)
 
-        #обновить весовые коэффициенты для связей между скрытым и выходным слоями
+        # обновить весовые коэффициенты для связей между скрытым и выходным слоями
         self.who += self.learningRate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
                                                   numpy.transpose(hidden_outputs))
 
-        #обновить весовые коэфициенты для связей между входным и скрытым слоями
+        # обновить весовые коэфициенты для связей между входным и скрытым слоями
         self.wih += self.learningRate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
                                                   numpy.transpose(hidden_outputs))
         pass
 
     def query(self, inputs_list):
-        #преобразовать список входящих значений в двумерный массив
+        # преобразовать список входящих значений в двумерный массив
         inputs = numpy.array(inputs_list, ndmin=2).T
 
-        #расчитать входящие сигналы для скрытого слоя
+        # расчитать входящие сигналы для скрытого слоя
         hidden_inputs = numpy.dot(self.wih, inputs)
-        #расчитать исходящие сигналы для скрытого слоя
+        # расчитать исходящие сигналы для скрытого слоя
         hidden_outputs = self.activation_function(hidden_inputs)
 
-
-        #расчитать входящие сигналы для выходного слоя
+        # расчитать входящие сигналы для выходного слоя
         final_inputs = numpy.dot(self.who, hidden_outputs)
-        #расчитать исходящие сигналы для выходящего слоя
+        # расчитать исходящие сигналы для выходящего слоя
         final_outputs = self.activation_function(final_inputs)
 
-        return  final_outputs
+        return final_outputs
         pass
     pass
 
